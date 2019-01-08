9aa255bc181ed967c0303f560cf7b8a45a70a429
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-01-08 15:16:28 -0600

    New palette.

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,40 +3,40 @@ import { ThemeInterface } from "./styled-components";
 const Theme: ThemeInterface = {
   colors: {
     grey: {
-      0: "#262f3a",
-      1: "#364252",
-      2: "#4d5f75",
-      3: "#7086a1",
-      4: "#9faec0",
-      5: "#dae0e7",
-      6: "#f7f8fa"
+      0: "#111212",
+      1: "#171A1D",
+      2: "#262F3A",
+      3: "#7C91A8",
+      4: "#C3D9EF",
+      5: "#E3F2FE",
+      6: "#F8FCFF"
     },
     primary: {
-      0: "#0f2c41",
-      1: "#246aa2",
-      2: "#4093d4",
+      0: "#133763",
+      1: "#275D95",
+      2: "#4687C0",
       3: "#70AEDF",
-      4: "#a0c9ea",
-      5: "#c0dbf1",
-      6: "#f0f7fc"
+      4: "#8ECAF1",
+      5: "#B7E3FB",
+      6: "#E6F7FF"
     },
     secondary: {
-      0: "#513801",
-      1: "#9f6d02",
-      2: "#eda202",
+      0: "#633313",
+      1: "#9F5C19",
+      2: "#D48E1F",
       3: "#FDBB2D",
-      4: "#fdc854",
-      5: "#fee7b5",
-      6: "#fffaef"
+      4: "#FFE761",
+      5: "#FFFDA2",
+      6: "#FFFFE6"
     },
     tertiary: {
-      0: "#6b1a12",
-      1: "#9d261a",
-      2: "#dc3a2b",
+      0: "#471511",
+      1: "#852C23",
+      2: "#BC483B",
       3: "#E4695D",
-      4: "#ea887f",
-      5: "#f2b7b1",
-      6: "#fdf5f5"
+      4: "#F69986",
+      5: "#FECBBA",
+      6: "#FFF7F4"
     }
   },
   space: [
